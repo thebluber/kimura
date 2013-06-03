a314260334edd3762e4325c969a5abc62c70e823
thebluber <thebluber@gmail.com>
2013-06-03 19:45:12 +0200

    add deploy

---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+# -*- encoding : utf-8 -*-                                                                                             
+set :deploy_to, "/srv/http/japo/kimura_staging"
+set :branch, 'staging'
+
