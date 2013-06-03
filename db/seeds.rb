b68fb41dcf374947eb08fe873b75fcf8ca855af7
thebluber <thebluber@gmail.com>
2013-06-03 20:08:36 +0200

    commet seeds out

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-File.open("./wadokus.txt", "r").each_line do |line|
+#File.open("./wadokus.txt", "r").each_line do |line|
 
 
-  twoArgs = line.split(/\t/)
-  entry = String.new
-  content = String.new  
-  
-  entry = twoArgs[0].gsub(/\s+/, "")
-  content = twoArgs[1].strip
-  
-   unless   twoArgs[0].include?("0000000")     
-   Wadoku.create([{:entry => "#{entry}", :contents => "#{content}"}])
-end
- 	  
-  	  	  end
+# twoArgs = line.split(/\t/)
+#entry = String.new
+#content = String.new  
+
+#entry = twoArgs[0].gsub(/\s+/, "")
+#content = twoArgs[1].strip
+
+#unless   twoArgs[0].include?("0000000")     
+#Wadoku.create([{:entry => "#{entry}", :contents => "#{content}"}])
+#end
+
+# 	  	  end
