fa24aa084cb830e1e7f97f6fabdafe91502a26f9
thebluber <thebluber@gmail.com>
2013-06-10 12:41:50 +0200

    changed layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+
+  end
+  def after_sign_out_path_for(resource)
+     user_session_path
+  end
+
 end
